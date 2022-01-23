1a302a6474d25b0a4ea676b226d27f414ae95ccd
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-01-22 23:24:00 -0300

    test: ensure Logo will render with correct Color

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
-
 import Logo from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo color="black" />)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
+
+/**
+ * Etapas de um teste:
+ * 1. Renderizar o componente 'render'
+ * 2. Selecionar o elemento a ser testado 'screen' (queries) - getByLabel..
+ * 3. expect -assertion - comparação - análise
+ */
