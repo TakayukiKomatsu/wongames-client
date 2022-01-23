0bf548cbbf03bcdf3fca0d721a7c26ff184c5d6b
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-01-23 00:01:23 -0300

    test: ensure Logo will render without text when hideOnMobile is true and media query matches

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,3 +1,5 @@
+import 'jest-styled-components'
+
 import Logo from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
 import { screen } from '@testing-library/react'
@@ -30,6 +32,17 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text on mobile if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
 
