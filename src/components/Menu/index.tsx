5945480464a5c139b95d50daa81b7c76d2064555
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-01-24 12:08:44 -0300

    feat: ensure Menu will render MenuFill wben isOpen is true

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,30 +1,49 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-import Logo from 'components/Logo'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
+import Logo from 'components/Logo'
 
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <S.Wrapper>
+      <S.IconWrapper
+        onClick={() => {
+          setIsOpen(true)
+        }}
+      >
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
 
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open shopping cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open shopping cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon
+          aria-label="Close Menu"
+          onClick={() => {
+            setIsOpen(false)
+          }}
+        ></CloseIcon>
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
